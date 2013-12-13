de79b62b176a51ee02990c17e4118857bb26ae2a
Asyncronous <asyncronous16@gmail.com>
2013-12-13 00:21:33 -0500

    created skeleton of exoskeleton

---
+++ exo/com/exo/Exoskeleton.java
@@ -0,0 +1,10 @@
+package com.exo;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid="EXO", name="Exoskeleton", version="0.0.0")
+@NetworkMod(channels={"EXO"}, clientSideRequired=true, serverSideRequired=false)
+public class Exoskeleton{
+	
+}
