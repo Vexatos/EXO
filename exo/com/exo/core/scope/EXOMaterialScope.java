74a9daba9d45ba66f473fd18756fedc11838f090
Asyncronous <asyncronous16@gmail.com>
2013-12-19 12:26:45 -0500

    Updating Repository

--- exo/com/exo/core/scope/EXOMaterialScope.java
+++
@@ -1,8 +0,0 @@
-package com.exo.core.scope;
-
-import net.minecraft.item.EnumArmorMaterial;
-import net.minecraftforge.common.EnumHelper;
-
-public interface EXOMaterialScope{
-	public static final EnumArmorMaterial EXO_SUIT_MATERIAL = EnumHelper.addArmorMaterial("exoSuit", 10, new int[]{0, 0, 0, 0}, 0);
-}
--- exo/com/exo/server/CommonProxy.java
+++
@@ -1,7 +0,0 @@
-package com.exo.server;
-
-public class CommonProxy{
-	public void initTiles(){}
-	public void initRenders(){}
-	public void init(){}
-}
